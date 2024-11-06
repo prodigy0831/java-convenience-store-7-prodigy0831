cc5d86719aa68e0f94a5b3c94a5049b003b97e9f
Beomseok Noh <prodigy0831@naver.com>
2024-11-06 21:48:18 +0900

    test(OutputViewTest): ProductRepository의 재고를 출력하는 OutputView 클래스 테스트

---
+++ src/test/java/view/OutputViewTest.java
@@ -0,0 +1,23 @@
+package view;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import repository.ProductRepository;
+
+public class OutputViewTest {
+    private static ProductRepository productRepository;
+    @BeforeAll
+    static void setUp(){
+        productRepository = new ProductRepository();
+
+    }
+
+    @Test
+    @DisplayName("출력 확인")
+    void test(){
+        assertThat(productRepository.getProducts().size()).isEqualTo(16);
+    }
+}
