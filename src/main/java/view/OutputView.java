1dc166d7ccc5113ff1cf74162396fdd183cd181d
Beomseok Noh <prodigy0831@naver.com>
2024-11-07 11:05:34 +0900

    feat(OutputView): 재고 출력시 금액,수량 포맷을 세자리마다 쉼표 추가

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,12 +1,23 @@
 package view;
 
 import domain.Product;
+import java.text.NumberFormat;
+import java.util.Objects;
 import repository.ProductRepository;
 
 public class OutputView {
     public static void printProducts(ProductRepository productRepository) {
-        for(Product product: productRepository.getProducts()){
-            System.out.println("- "+product.getName()+" "+product.getPrice()+"원 "+product.getQuantity()+"개 "+product.getPromotion());
+        NumberFormat numberFormat = NumberFormat.getInstance();
+        for (Product product : productRepository.getProducts()) {
+            String promotion = "";
+            if (!Objects.equals(product.getPromotion(), "null")) {
+                promotion = product.getPromotion();
+            }
+
+            String formattedPrice = numberFormat.format(product.getPrice());
+            String formattedQuantity = numberFormat.format(product.getQuantity());
+            System.out.printf("- %s %s원 %s개 %s%n", product.getName(), formattedPrice, formattedQuantity,
+                    promotion);
         }
     }
 }
