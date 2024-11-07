d2d1b756626ff98542fa187d36fe1c4f8624137e
Beomseok Noh <prodigy0831@naver.com>
2024-11-07 12:30:45 +0900

    test(InputHandlerTest):InputHandler클래스 input테스트

---
+++ src/test/java/service/InputHandlerTest.java
@@ -0,0 +1,29 @@
+package service;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import repository.ProductRepository;
+
+public class InputHandlerTest {
+    private static ProductRepository productRepository;
+    @BeforeAll
+    static void setUp(){
+        productRepository = new ProductRepository();
+    }
+
+    @ParameterizedTest
+    @NullAndEmptySource
+    @ValueSource(strings = {"[]"})
+    @DisplayName("사용자 입력이 비어있거나 null 또는 공백일 때")
+    void EmptyOrNullInputTest(String input){
+        assertThatThrownBy(()->{
+            InputHandler.processInput(input,productRepository);
+        } ).isInstanceOf(IllegalArgumentException.class);
+
+    }
+}
